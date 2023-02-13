20290c4d3edb4626c5d954a74b197bc3bd705230
hoti <code@feedyourhead.at>
2023-02-13 21:57:11 +0100

    fixed cachdir creation

--- src/deserialize.rs
+++ src/deserialize.rs
@@ -2,6 +2,8 @@ use std::fs::File;
 use std::fs;
 use std::io::prelude::*;
 use serde::Deserialize;
+use std::path::Path;
+use std::env;
 
 #[derive(Deserialize, PartialEq, Debug)]
 pub struct Config {
@@ -29,15 +31,33 @@ pub fn load_file(file: &str) -> Result<(), serde_yaml::Error>  {
 
     let cachedir = match ymlconfig.cachedir {
         Some(ref x) => x,
-        None => "~/.cache/dblfetch"
+        None => ".cache/dblfetch"
     };
 
-    let cachedir = fs::canonicalize(cachedir).unwrap_or_else(|e| panic!("Error canonicalize: {} {}", e, cachedir));
-    fs::create_dir_all(cachedir).unwrap_or_else(|e| panic!("Error creating dir: {}", e));
+    let cachedir_path = default_cachedir(cachedir);
+
+    println!("Cache-Dir: {}", cachedir_path);
+
+    fs::create_dir_all(cachedir_path).unwrap_or_else(|e| panic!("Error creating dir: {}", e));
+
+
 
     for dbl in ymlconfig.blacklists {
         println!("Got: {}", dbl.name);
     }
 
     Ok(())
+}
+
+fn default_cachedir(file: &str) -> String {
+    let home = "HOME";
+    
+    let homepath = match env::var(home) {
+        Ok(val) => val,
+        Err(e) => panic!("Error: could not find {}: {}", home, e),
+    };
+
+    let fullpath: String = Path::new(&homepath).join(file).into_os_string().into_string().unwrap();
+
+    fullpath
 }
